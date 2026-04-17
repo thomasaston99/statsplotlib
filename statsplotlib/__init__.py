8f02f33caa9c9edd0860788d000617fe6cf2f05a
thomasaston99 <thomas.aston@statsports.com>
2026-04-17 12:47:13 +0100

    fix __init__.py not including subplotting

--- statsplotlib/__init__.py
+++ statsplotlib/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.1.0"
 __author__ = "Thomas Aston"
 
-from .plotting import create_branded_plot, add_branded_title
+from .plotting import create_branded_plot, create_branded_subplot, add_branded_title
